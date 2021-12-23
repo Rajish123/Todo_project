d2800d8d8bdaed19856d1948edf4bb48a33adeae
Rajish Maharjan <maharjanrajish101@gmail.com>
2021-12-23 16:44:12 +0545

    login page modified

--- Todo_list/models.py
+++ Todo_list/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
