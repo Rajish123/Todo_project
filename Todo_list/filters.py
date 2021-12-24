3d24c1bd7ac8866f52c1d81f4a7c61ac251e85ce
Rajish Maharjan <maharjanrajish101@gmail.com>
2021-12-24 20:30:22 +0545

    Charfilter and Datefilter added

--- Todo_list/filters.py
+++ Todo_list/filters.py
@@ -1,7 +1,10 @@
 import django_filters
+from django_filters import DateFilter,CharFilter
 from .models import *
 
 class SearchFilter(django_filters.FilterSet):
+    description = CharFilter(field_name = 'description', lookup_expr = 'icontains')
+    date = DateFilter(field_name = 'created_at', lookup_expr = 'lte')
     class Meta:
         model = Todo
         fields = ['title']
